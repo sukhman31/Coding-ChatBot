c80fdf8cf7dbf900ff1de722fe35ca43a6462dba
sukhman31 <ssingh8_be20@thapar.edu>
2022-12-27 17:37:58 +0530

    finished backend

--- server/server.js
+++ server/server.js
@@ -13,6 +13,40 @@ const configuration=new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = new express();
+//to allow cross origin requests
 app.use(cors());
+
+//to allow json to pass from frontend to backend
 app.use(express.json());
 
+//dummy route
+app.get('/',async(req,res)=>{
+    res.status(200).send({
+        message: 'Hello from Codex'
+    })
+});
+
+//get cant receive data from frontend so we make post
+app.post('/',async(req,res)=>{
+    try{
+        const prompt = req.body.propmt;
+
+        const response = await openai.createCompletion({
+            model:"text-davinci-003",
+            prompt:`${prompt}`,
+            temperature:0,
+            max_tokens:3000,
+            top_p:1,
+            frequency_penalty:0.5,
+            presence_penalty:0,
+        });
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch(error){
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
+app.listen(5000, ()=> console.log('Server is running on port 5000'));
